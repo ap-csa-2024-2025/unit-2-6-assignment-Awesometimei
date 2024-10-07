6ec8eac017e008e2289ccb7c37bf4e69a565b134
Awesometimei <dtnguyen194@student.hbuhsd.edu>
2024-10-07 10:08:33 -0700

    baka

--- Main.java
+++ Main.java
@@ -4,6 +4,26 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    Scanner sc = new Scanner(System.in);
+    
+    //Problem 1
+    double pi = 3.14159265359;
+    System.out.println("Enter the radius of the circle");
+    double rad = sc.nextDouble();
+    Circle circ1 = new Circle(rad);
+    double circumference = 2*rad*pi;
+    double area = pi*rad*rad;
+    System.out.println(circ1.toString() + " has a circumference of " + circumference + " and an area of " + area + ".");
+
+    //Problem 2
+    System.out.println("Enter a number of sides: ");
+    int sideNum = sc.nextInt();
+    System.out.println("Enter a side length: ");
+    double sideLen = sc.nextDouble();
+    RegularPolygon regPoly = new RegularPolygon(sideNum, sideLen);
+    System.out.println("Area with " + regPoly.getNumSides() + " sides is " + regPoly.getArea());
+    regPoly.addSides(2);
+    System.out.println("Incrementing the number of sides by two");3
+    System.out.println("Area with " + regPoly.getNumSides()  + " sides is " + regPoly.getArea());
   }
 }
